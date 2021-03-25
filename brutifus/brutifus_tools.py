a2557c14c60f99b695fdc67d6ea856247d7594c0
13974615+fpavogt <13974615+fpavogt@users.noreply.github.com>
2021-03-25 12:11:09 +0100

    Fix license notice in all module docstrings

--- brutifus/brutifus_tools.py
+++ brutifus/brutifus_tools.py
@@ -3,6 +3,11 @@
 brutifus: a set of Python modules to process datacubes from integral field spectrographs.\n
 Copyright (C) 2018-2020,  F.P.A. Vogt
 Copyright (C) 2021, F.P.A. Vogt & J. Suherli
+All the contributors are listed in AUTHORS.
+
+Distributed under the terms of the GNU General Public License v3.0 or later.
+
+SPDX-License-Identifier: GPL-3.0-or-later
 
 This file contains general tools for the brutifus routines to fit the stellar continuum
 and the emission lines in an IFU data cube.
--- brutifus/brutifus_version.py
+++ brutifus/brutifus_version.py
@@ -3,6 +3,11 @@
 brutifus: a set of Python modules to process datacubes from integral field spectrographs.\n
 Copyright (C) 2018-2020,  F.P.A. Vogt
 Copyright (C) 2021, F.P.A. Vogt & J. Suherli
+All the contributors are listed in AUTHORS.
+
+Distributed under the terms of the GNU General Public License v3.0 or later.
+
+SPDX-License-Identifier: GPL-3.0-or-later
 
 This file contains the version of the code.
 
